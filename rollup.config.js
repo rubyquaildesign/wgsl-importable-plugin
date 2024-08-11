5fa32da3c0bbe131f364451c284541d1e0d8e10b
Ruby Quail <ruby@rubyquail.design>
2024-08-11 12:19:20 +1000

    ok all fixed
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- rollup.config.js
+++ rollup.config.js
@@ -2,23 +2,49 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescriptPlugin from 'rollup-plugin-ts';
 import { defineConfig } from 'rollup';
 import requireJS from '@rollup/plugin-commonjs';
-export default defineConfig({
-	input: ['src/index.ts'],
-	output: [
-		{
-			file: 'output/index.cjs',
-			format: 'cjs',
-		},
-		{
-			file: 'output/index.mjs',
-			format: 'esm',
-		},
-	],
-	plugins: [
-		nodeResolve(),
-		typescriptPlugin({ tsconfig: './tsconfig.json' }),
-		requireJS(),
-	],
+export default defineConfig([
+	{
+		input: 'src/index.ts',
+		output: [
+			{
+				format: 'esm',
+				entryFileNames: 'index.mjs',
+				dir: 'output',
+			},
+			{
+				format: 'commonjs',
+				entryFileNames: 'index.cjs',
+				dir: 'output',
+			},
+		],
+		plugins: [
+			nodeResolve(),
+			typescriptPlugin({ tsconfig: './tsconfig.json' }),
+			requireJS(),
+		],
 
-	external: [],
-});
+		external: [],
+	},
+	{
+		input: 'src/vite.ts',
+		output: [
+			{
+				format: 'esm',
+				entryFileNames: 'vite.mjs',
+				dir: 'output',
+			},
+			{
+				format: 'commonjs',
+				entryFileNames: 'vite.cjs',
+				dir: 'output',
+			},
+		],
+		plugins: [
+			nodeResolve(),
+			typescriptPlugin({ tsconfig: './tsconfig.json' }),
+			requireJS(),
+		],
+
+		external: [],
+	},
+]);
